1dbed5bb159b6423e220ad20dfe04c896b7175c8
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-08-17 13:42:13 -0300

    Ex03 Concluído

---
+++ src/1-Laços_de_repetição/Ex03.ts
@@ -0,0 +1,11 @@
+// Imprima os números pares de 0 a 20 utilizando um loop for
+
+export let exercicio3 = function exercicio3() {
+    for (let i = 0; i <= 20; i++) {
+        if (i % 2 == 0 && i != 0) {
+            console.log(i);
+        };
+    }
+};
+
+// OK
--- src/1-Laços_de_repetição/index.ts
+++ src/1-Laços_de_repetição/index.ts
@@ -1,21 +1,22 @@
 import { exercicio1 } from "./Ex01";
 import { exercicio2 } from "./Ex02";
+import { exercicio3 } from "./Ex03";
 
 const prompt = require("prompt-sync")();
 
 let exercicio: string;
 
-function iniciarPrograma() {2
+function iniciarPrograma() {
     console.log();
     console.log("=================================================");
     console.log("Bem vindo a lista de exercícios de laços de repetição!");
     console.log("Escolha qual exercício será executado: ");
     console.log("1 - Imprima os números de 1 a 10 utilizando um loop for");
     console.log("2 - Calcule a soma dos números de 1 a 100 utilizando um loop while");
-    console.log("2 - Imprima os números pares de 0 a 20 utilizando um loop for");
-    console.log("3 - Calcule o produto dos números de 1 a 5 utilizando um loop do...while");
-    console.log("4 - Crie um loop que imprima a sequência de Fibonacci até o 10º termo");
-    console.log("5 - Imprima os múltiplos de 3 de 0 a 30 utilizando um loop for");
+    console.log("3 - Imprima os números pares de 0 a 20 utilizando um loop for");
+    console.log("4 - Calcule o produto dos números de 1 a 5 utilizando um loop do...while");
+    console.log("5 - Crie um loop que imprima a sequência de Fibonacci até o 10º termo");
+    console.log("6 - Imprima os múltiplos de 3 de 0 a 30 utilizando um loop for");
     exercicio = prompt("Digite o número: ");
 
     switch (exercicio) {
@@ -25,16 +26,16 @@ function iniciarPrograma() {2
         case "2":
             exercicio2();
             break;
-        case "2":
-
-            break;
         case "3":
-
+            exercicio3();
             break;
         case "4":
 
             break;
         case "5":
+
+            break;
+        case "6":
 
             break;
 
