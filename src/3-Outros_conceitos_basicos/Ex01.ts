07af69e97e12710e387c914f76c0b8d70e93e58c
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-08-18 14:42:26 -0300

    Ex01 Concluído

--- src/3-Outros_conceitos_basicos/Ex01.ts
+++ src/3-Outros_conceitos_basicos/Ex01.ts
@@ -1,5 +1,21 @@
 // Crie uma função que calcule o fatorial de um número inteiro
 
+import { prompt } from "..";
+
 export function exercicio1() {
-    console.log("Ex1");
+    console.log("Programa que cria uma função que calcula o fatorial de um número inteiro...");
+
+    let numero = prompt("Digite um número: ");
+    let somaFatorial = 1;
+
+    // 1 * 2 = 2
+    // 2 * 3 = 6
+    // 6 * 4 = 24
+    // 24 * 5 = 120
+    for (let i = 2; i <= numero; i++) {
+        somaFatorial = somaFatorial * i;
+        console.log(somaFatorial);
+    }
 }
+
+// OK
