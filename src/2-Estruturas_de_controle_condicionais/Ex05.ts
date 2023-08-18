a2ad06c9907f9d96191e27cc05c87e8ac4c1d6cc
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-08-17 23:13:19 -0300

    Ex05 Concluído

--- src/2-Estruturas_de_controle_condicionais/Ex05.ts
+++ src/2-Estruturas_de_controle_condicionais/Ex05.ts
@@ -1,3 +1,22 @@
+import { prompt } from "..";
+
 export function exercicio5() {
     console.log("Programa que verifica se uma string é um palíndromo (lê-se da mesma forma de trás para frente)...");
+
+    let listaInversa = [];
+    let listaInversaString;
+
+    let palavra = prompt("Digite uma palavra qualquer ou um palíndromo: ").toLowerCase();
+    for (let i = palavra.length - 1; i >= 0; i--) {
+        listaInversa.push(palavra[i]);
+    }
+    listaInversaString = listaInversa.join("");
+
+    if ( palavra === listaInversaString) {
+        console.log(`A palavra ${palavra} é um Palíndromo!`);
+    } else {
+        console.log(`A palavra ${palavra} não é um Palíndromo!`)
+    }
 }
+
+// OK
