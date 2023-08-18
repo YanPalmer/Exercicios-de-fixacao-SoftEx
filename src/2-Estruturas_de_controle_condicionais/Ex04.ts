6d991ef71556fa8a76372d4db99efed4026664b8
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-08-17 21:17:54 -0300

    Ex04 Concluído

--- src/2-Estruturas_de_controle_condicionais/Ex04.ts
+++ src/2-Estruturas_de_controle_condicionais/Ex04.ts
@@ -1,3 +1,36 @@
+import { prompt } from "..";
+
 export function exercicio4() {
     console.log("Programa que cria uma calculadora simples que opera com dois números e um operador (+, -, *, /)...");
+
+    let valor1 = parseInt(prompt("Digite um valor: "));
+    let valor2 = parseInt(prompt("Digite outro valor:"));
+    console.log("Escolha um dos operadores a seguir:");
+    console.log("1 - (+)");
+    console.log("2 - (-)");
+    console.log("3 - (/)");
+    console.log("4 - (*)");
+    let operador = prompt("Número do operador: ");
+
+    switch (+operador) {
+        case 1:
+            console.log(valor1 + valor2);
+            break;
+        case 2:
+            console.log(valor1 - valor2);
+            break;
+        case 3:
+            console.log(valor1 / valor2);
+            break;
+        case 4:
+            console.log(valor1 * valor2);
+            break;
+        default:
+            console.log("Teste");
+            break;
+    }
+    console.log();
+
 }
+
+// OK
