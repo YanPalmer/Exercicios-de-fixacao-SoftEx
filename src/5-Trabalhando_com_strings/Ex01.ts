eaa7588336ba4cad6e4e717a8afe1fcb5638c3b0
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-08-19 21:14:33 -0300

    Ex01 Concluído

--- src/5-Trabalhando_com_strings/Ex01.ts
+++ src/5-Trabalhando_com_strings/Ex01.ts
@@ -1,5 +1,30 @@
 // Conte quantas vogais há em uma string
 
+import { prompt } from "..";
+
 export function exercicio1() {
-    console.log("Teste");
+    console.log("Programa que conta quantas vogais há em uma string");
+
+    // Exemplo: yan
+    const palavra: string = prompt("Digite uma palavra qualquer: ");
+    const vogais: string = "aeiouAEIOU";
+
+    function verificarVogais() {
+        console.log();
+        let quantidadeDeVogais: number = 0;
+        let vogaisEncontradas: string[] = [];
+        
+        for (const letra of palavra) {
+            if (vogais.includes(letra)) {
+                quantidadeDeVogais++;
+                vogaisEncontradas.push(letra);
+            }
+        }
+        console.log(`A palavra "${palavra}" contem:`);
+        console.log(`${quantidadeDeVogais} vogais e foram encontradas as vogais: ${vogaisEncontradas.join(", ")}`)
+    }
+
+    verificarVogais();
 }
+
+// OK
