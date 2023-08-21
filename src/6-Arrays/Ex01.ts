7cbf376e7636a09caf5f6e7a361255c6d16fd913
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-08-21 15:10:39 -0300

    Código quase pronto
    Terminarei o código assim que chegar em casa anoite. Parei no loop while para verificar qual o menor valor

--- src/6-Arrays/Ex01.ts
+++ src/6-Arrays/Ex01.ts
@@ -1,5 +1,29 @@
 // Crie um array de números e ordene-o em ordem crescente
 
+import { prompt } from ".."
+
 export function exercicio1() {
-    console.log("Teste");
+    console.log("Programa que cria um array de números e ordene-o em ordem crescente...")
+
+    function inserirArrayDeNumeros() {
+        const arrayDeNum: number[] = [];
+        const quantidadeDeNumeros: number = prompt("Digite quantos números você deseja inserir: ");
+        let entrada: number;
+
+        for (let i = 0; i < quantidadeDeNumeros; i++) {
+            entrada = prompt(`Digite a ${i + 1}º entrada:`);
+            arrayDeNum.push(entrada);
+        }
+        return arrayDeNum;
+        return quantidadeDeNumeros;d
+    }
+
+    function ordenarNumeros(arrayDeNum: number[], quantidadeDeNumeros: number) {
+        while () {
+            
+        }
+    }
+
+    
+    // console.log(inserirArrayDeNumeros());
 }
